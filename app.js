fdf71a8e613361df9c22a9ddd2c997f8ff9a95a0
kevinantunezortiz <kevinantunetiz@gmail.com>
2025-01-21 14:56:24 -0600

    se añadio la función sortearAmigo()

--- app.js
+++ app.js
@@ -22,3 +22,18 @@ function actualizarLista(){
         lista.appendChild(elemento);
     });
 }
+
+function sortearAmigo() {
+    if(amigos.length>0){
+        const posicion = Math.floor(Math.random()*amigos.length);
+        const amigoSorteado = amigos[posicion];
+        console.log(amigoSorteado);
+        const listaSeleccionado = document.getElementById("resultado");
+        listaSeleccionado.innerHTML="";
+        const elemento = document.createElement("li");
+        elemento.innerText=`El amigo sorteado secreto es ${amigoSorteado}`;
+        listaSeleccionado.appendChild(elemento);
+    }else{
+        alert("Aun no tienes agregados amigos");
+    }
+}
