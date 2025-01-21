5b8c707d1c3463fced4f709a3b88b5aec19067eb
kevinantunezortiz <kevinantunetiz@gmail.com>
2025-01-21 14:54:59 -0600

    se añadio la función agregarAmigo()

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    let inputAmigo = document.getElementById("amigo");
+    const amigo = inputAmigo.value;
+    if(amigo!==""){
+        amigos.push(amigo);
+        actualizarLista();
+        inputAmigo.value="";
+    }else{
+        alert("Debes escribir el nombre de uno de tus amigos");
+    }
+}
